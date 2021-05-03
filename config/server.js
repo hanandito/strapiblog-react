98bb79659729641b5210aaf1e8475da108f4be44
teddypicker <hananditos@gmail.com>
2021-05-03 10:45:51 +0700

    first shot

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a248d238f6ae4a19a1986a408ca37009'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9f96de36-9354-47b4-9ee4-8cef30281343'
+};
